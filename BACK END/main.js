db42ee2a65b8ecdf3fefd663bbda101a73b9ea56
boter116 <rrespici@sfu.ca>
2020-02-08 11:13:18 -0800

    finally works git add .git add .git add .!

--- BACK END/main.js
+++ BACK END/main.js
@@ -66,6 +66,7 @@ function getAccessToken(oAuth2Client, callback) {
   });
 }
 
+
 /**
  * Lists the next 10 events on the user's primary calendar.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
@@ -92,3 +93,38 @@ function listEvents(auth) {
     }
   });
 }
+
+
+//Sets the event metadata to event
+/* var event = {
+  'summary': 'Google I/O 2020',
+  'location': '800 Howard St., San Francisco, CA 94103',
+  'description': 'A chance to hear more about Google\'s developer products.',
+  'start': {
+    'dateTime': '2020-05-28T09:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'end': {
+    'dateTime': '2020-05-28T17:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'recurrence': [
+    'RRULE:FREQ=DAILY;COUNT=2'
+  ],
+  'attendees': [
+    {'email': 'lpage@example.com'},
+    {'email': 'sbrin@example.com'},
+  ],
+  'reminders': {
+    'useDefault': false,
+    'overrides': [
+      {'method': 'email', 'minutes': 24 * 60},
+      {'method': 'popup', 'minutes': 10},
+    ],
+  },
+};
+
+function insertEvent(auth) {
+  const calendar = google.calendar({version: 'v3', auth});
+}
+ */
