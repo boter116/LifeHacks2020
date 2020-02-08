f646a0af5dbbd6035b439fa40cb403fa4ac808b9
RylanFerrer <rylanferrer@gmail.com>
2020-02-08 13:58:37 -0800

    added fetching to backend

--- chrome_extension/selection.js
+++ chrome_extension/selection.js
@@ -1,30 +1,34 @@
 const monthReg = [new RegExp('(((j|J)an)(uary)?)'),new RegExp('(((f|F)eb)(ruary)?)'),new RegExp('(((m|M)ar)(ch)?)'),
     new RegExp('(((a|A)pr)(il)?)'),new RegExp('((m|M)ay)'),new RegExp('(((j|J)un)e?)'),new RegExp('(((j|J)ul)(y)?)'), 
     new RegExp('(((a|A)ug)(ust)?)'),new RegExp('(((s|S)ept)(ember)?)'),new RegExp('(((o|O)ct)(ober)?)'),
-    new RegExp('(((n|N)ov)(ember)?)'), new RegExp('((((d|D)ec)(ember)?)')]; // regex for different ways of writing month
+    new RegExp('(((n|N)ov)(ember)?)'), new RegExp('(((d|D)ec)(ember)?)')]; // regex for different ways of writing month
 
 
 const months= ["january","february","march","april","may","june","july",
     "august","september","october","november","december"];
 const durations = ["week", "month", "day", "weeks","months", "days", "year", "years"]
 
-function saveEvent(event) {
+async function saveEvent(event) {
     if(filterDate(event.selectionText)) {
-        // api call
-    }
-}
+        try{
+            await fetch('http://localhost:5000/')
+            alert('success')
+        } catch(e) {
+            alert('err')
+        }
+}}
 
 
 // true if date is format "Month xx" or "month" or "mon" or "mon xx"
 function filterDate(text) {
     text = convertDate(text);
     console.log(text);
-    if( ((new Date(event.selectionText) !== "Invalid Date") && !isNaN(new Date(event.selectionText)) 
-        || months.some(month => event.selectionText.toLowerCase().includes(month))))
+    if( ((new Date(text) !== "Invalid Date") && !isNaN(new Date(text)) 
+        || months.some(month => text.toLowerCase().includes(month))))
     {
         return true;
     } 
-    else if (durations.some((duration) => event.selectionText.toLowerCase().includes(duration) === true )) 
+    else if (durations.some((duration) => text.toLowerCase().includes(duration) === true )) 
     {
         return true;
     }
@@ -37,8 +41,8 @@ function filterDate(text) {
 // if accepted by regex returns changed month if rejected, return self
 function convertDate(monthtext) {
     for (let i = 0; i < 12; i++) {
-        if (monthReg.test(monthtext))
-            return month[i];
+        if (monthReg[i].test(monthtext))
+            return months[i];
     }
     return monthtext;
 }
