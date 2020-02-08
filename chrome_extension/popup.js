e3a4c7385eee2ef670f866262d16602ad3dfaa11
RylanFerrer <rylanferrer@gmail.com>
2020-02-08 10:58:36 -0800

    got this to finnally work

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,8 +1,25 @@
+window.addEventListener('load', (event) => {
+    chrome.tabs.executeScript(null, {
+      file: 'selection.js',  }, () => {
+        connect()   });
+    });
+
+
+  function connect() {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const port = chrome.tabs.connect(tabs[0].id);
+      port.postMessage({ function: 'html' });
+      port.onMessage.addListener((response) => {
+        html = response.html;
+        title = response.title;
+        description = response.description;
+      });
+    });
+  }
+  
 const id = document.getElementById('paste')
 console.log(id)
- id.addEventListener('onclick', (event) => {
-     pasteHighlight();
- })
+ id.addEventListener('click', (event) => pasteHighlight())
 function pasteHighlight() {
     chrome.tabs.query({active:true, windowId: chrome.windows.WINDOW_ID_CURRENT},
         tab => {
@@ -12,4 +29,3 @@ function pasteHighlight() {
             });
         });
 }
-
--- chrome_extension/selection.js
+++ chrome_extension/selection.js
@@ -1,3 +1,4 @@
+
 chrome.extension.onMessage.addListener((request, sender, sendResponse) =>{
     if (request.method == "getSelection")
         sendResponse({data: window.getSelection().toString()});
