a7e6f6f58dc1ede8eb8b001ebae7c514b5491b09
RylanFerrer <rylanferrer@gmail.com>
2020-02-08 14:54:04 -0800

    added connection to front and back end

--- BACK END/server.js
+++ BACK END/server.js
@@ -1,14 +1,17 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const cors = require("cors")
 const PORT = process.env.PORT || 5000
-app.use(express.json())
+app.use(express.json({
+    type: ['application/json', 'text/plain']
+  }))
 app.post('/', (req,res) => {
     const {word} = req.body
-    
+    console.log(req.body)
 
 })
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
